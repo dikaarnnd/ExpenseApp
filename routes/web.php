368746879c7e47b02d2dd54ba90a485e47ec9820
UTOPi4T <wsense911@gmail.com>
2024-11-17 21:57:28 +0700

    about page

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return "hello Dika";
+Route::get('/about', function () {
+    return view('about');
 });
 
